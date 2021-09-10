be57de391b00304cf6987ffd72caa80582c01662
Trung Nguyen <24930+trung@users.noreply.github.com>
2021-09-10 11:05:08 -0400

    customize smtp server host for sample

--- src/sample.ts
+++ src/sample.ts
@@ -2,28 +2,27 @@ import {port} from "./config";
 import {logger} from "./logger";
 import {createTransport} from "nodemailer";
 
-let transporter = createTransport({
-    host: "localhost",
-    port: port,
-    secure: false,
-    requireTLS: false,
-    tls: {
-        rejectUnauthorized: false,
-    },
-    auth: {
-        user: "abc",
-        pass: "foo",
-    }
-});
-
 async function main() {
+    const transporter = createTransport({
+        host: process.env.SERVER || "localhost",
+        port: port,
+        secure: false,
+        requireTLS: false,
+        tls: {
+            rejectUnauthorized: false,
+        },
+        auth: {
+            user: "abc",
+            pass: "foo",
+        }
+    });
     const arbitraryMailOptions = {
         from: "no-reply@example.com", // sender address
         to: process.env.TO || "foo@example.com", // list of receivers
         subject: "Arbitrary subject", // Subject line
         html: "<b>Hello world?</b>", // html body
     };
-    let result = await transporter.sendMail(arbitraryMailOptions);
+    const result = await transporter.sendMail(arbitraryMailOptions);
 
     logger.info(`Mail sent. ID: ${result.messageId}`);
 }
